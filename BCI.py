f4a9605f78f0cd921cae4ce39ce5a04ada92df1f
Isaac Overcast <isaac.overcast@gmail.com>
2024-04-17 10:50:13 -0400

    Auto-calculate pi for 97% clusters, and allow plot_multi to color by sample name substring

--- BCI.py
+++ BCI.py
@@ -1,11 +1,15 @@
 import argparse
+import glob
 import joblib
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
 import random
+import shutil
 import subprocess
+from collections import Counter
+from itertools import combinations
 from matplotlib import cm
 from scipy.stats import entropy
 
@@ -39,9 +43,13 @@ class BCI:
         self._results = {}
         self._verbose = verbose
 
+        self._min_clust_threshold = 80
 
+
+    # FIXME: Is swarm better here? It works quite differently, and wouldn't work
+    #        well with ASV-table-like data (because it needs sequence counts).
     def _build_cmds(self):
-        self.tols = np.linspace(100, 80, 20)/100
+        self.tols = np.arange(100, self._min_clust_threshold, -1)/100
         self.cmds = []
         self.seed_files = []
         for tol in self.tols:
@@ -64,7 +72,8 @@ class BCI:
 
     ## FIXME: Add a check that vsearch is installed
     ## FIXME: Make n_jobs dynamic
-    def run(self, verbose=False):
+    ## FIXME: Auto-calculate nucleotide diversity at 97% clustering
+    def run(self, simulated=False, verbose=False):
         # Build the list of vsearch commands
         self._build_cmds()
         # Run all vsearch commands in parallel
@@ -79,21 +88,25 @@ class BCI:
         self._results.setdefault(self._label, [])
         self._results[self._label].append(self.bci)
 
+        try:
+            self.nucleotide_diversity(simulated=simulated, verbose=verbose)
+        except Exception as inst:
+            raise
 
-    def plot(self, ax=None, log=True, normalize=True, **kwargs):
+    def plot(self, ax=None, log=True, normalize=False, **kwargs):
         if not ax:
             fig, ax = plt.subplots(figsize=(8, 8))
         fig = ax.get_figure()
         # log transform
-        dat = np.log(self.bci) if log else self.bci
+        dat = np.log(self.bci) if log else np.array(self.bci)
         # normalize
-        norm = dat[0] if normalize else 1
+        norm = dat.sum() if normalize else 1
         ax.plot(np.array(dat)/norm, label=self._label, **kwargs)
         ax.legend(loc='upper right', bbox_to_anchor=(0.97, 0.97))
         return fig, ax
 
 
-    def plot_all(self, ax=None, log=True, normalize=True, cmap="Spectral", **kwargs):
+    def plot_all(self, ax=None, log=True, normalize=False, cmap="Spectral", **kwargs):
         if not ax:
             fig, ax = plt.subplots(figsize=(8, 8))
         fig = ax.get_figure()
@@ -107,7 +120,7 @@ class BCI:
                 # log transform
                 dat = np.log(bci) if log else bci
                 # normalize
-                norm = dat[0] if normalize else 1
+                norm = dat.sum() if normalize else 1
                 ax.plot(np.array(dat)/norm, label=label, color=cdict[label], **kwargs)
         ax.legend(loc='upper right', bbox_to_anchor=(0.97, 0.97))
         return fig, ax
@@ -119,6 +132,8 @@ class BCI:
         raw - Reset data to clean raw format
         disturbance - Downsample the # of sequences to simulate a disturbance
         invasion - Randomly sample one individual and replace a fraction of the community with it
+
+        fraction is the proportion of the community to transform by the disturbance
         """
         # Rewind any transformations applied and revert to the raw data
         self.data = self._data
@@ -137,7 +152,7 @@ class BCI:
 
                     if transformation == "disturbance":
                         for x in seqdealer:
-                            if random.random() < fraction:
+                            if random.random() > fraction:
                                 outfile.write(''.join(x))
                     else:
                         invader = next(seqdealer)
@@ -162,34 +177,207 @@ class BCI:
         return(stdout, stderr, retval)
 
 
+    def nucleotide_diversity(self, OTU_threshold=0.97, simulated=False, verbose=False):
+        """
+        Calculate nucleotide diversity per species/OTU for the entire dataset.
+
+        simulated - Whether the input fasta is simulated or empirical. If it's
+                    simulated then the sequences are organized into known species
+                    and we can calculate pi for both the known simulated species
+                    identies and also the 97% OTUs (for comparison).
+        """
+        def pi_from_fasta(data):
+            """
+            An inner function for calculating pi from an input fasta file.
+            The nested function is so we can call it multiple times for simulated
+            data on both the raw file and clustered OTU file.
+            """
+            with open(data) as infile:
+                #Drop the trailing newline
+                dat = infile.read().strip().split()
+            # Simulated data species ids are of the form >r0_x
+            self.pis = {x:0 for x in set([x.rsplit("_", 1)[0][1:] for x in dat[::2]])}
+            # Set the index to the species id
+            # Creates a df of the form:
+            #   species_ID_1    ACGTC...
+            #   species_ID_1    ACGGC...
+            #   species_ID_2    CCGGC
+            seq_df = pd.DataFrame(dat[1::2], index=[x.rsplit("_", 1)[0][1:] for x in dat[::2]], columns=["seqs"])
+            for spid in self.pis.keys():
+                self.pis[spid] = self._nucleotide_diversity(seq_df.loc[spid]["seqs"])
+
+        if simulated:
+            ## Only want to do this for simulated data because empirical data doesn't have
+            ## known species membership
+            pi_from_fasta(self.data)
+            self.sim_pis = self.pis.copy()
+
+        aligned = self._align_OTUs(OTU_threshold=OTU_threshold, verbose=verbose)
+        pi_from_fasta(aligned)
+
+
+    ## FIXME: This should be in a util or stats package
+    def _nucleotide_diversity(self, seqs, verbose=False):
+        """
+        Calculate nucleotide diversity from a list of sequences.
+        `seqs` input should be a list of aligned sequences
+        """
+        pi = 0
+
+        ## If no sequences or no variation
+        if len(seqs) <= 1: return 0
+
+        ## Transpose, so now we have a list of lists of all bases at each
+        ## position.
+        dat = np.transpose(np.array([list(x) for x in seqs]))
+
+        ## for each position
+        for d in dat:
+            ## If the position is _not_ monomorphic
+            if len(Counter(d)) > 1:
+                if verbose: print(Counter(d))
+                ## Enumerate the possible comparisons and for each
+                ## comparison calculate the number of pairwise differences,
+                ## summing over all sites in the sequence.
+                base_count = Counter(d)
+                ## ignore indels
+                del base_count["-"]
+                del base_count["N"]
+                for c in combinations(base_count.values(), 2):
+                    #print(c)
+                    n = c[0] + c[1]
+                    n_comparisons = float(n) * (n - 1) / 2
+                    pi += float(c[0]) * (n-c[0]) / n_comparisons
+        return pi/len(seqs.iloc[0])
+
+
+    def _align_OTUs(self, OTU_threshold=0.97, verbose=False):
+        # Read the utmp file to get hits matching to seeds
+        # Retain only columns 0 (hits) and 1 (seeds). Set the index to the seed names
+        utmp = glob.glob(self.tmpdir+f"/*{OTU_threshold}*.utmp")[0]
+        if not utmp:
+            # Something happened, no utmp file
+            raise Exception("No utmp file found with OTU_threshold: {OTU_threshold}")
+
+        # Make a new tmp directory to contain the aligned fasta files
+        # Force clean up the old directory if it exists
+        tmp_fastadir = self.tmpdir+f"/OTU-{OTU_threshold}_fastas"
+        shutil.rmtree(tmp_fastadir, ignore_errors=True)
+        os.mkdir(tmp_fastadir)
+
+        clusts = pd.read_csv(utmp, sep="\t", header=None, usecols=[0,1], index_col=1)
+        otus = set(clusts.index)
+
+        # Get a data frame formatted with the zotu name as the index, like this:
+        #   zotu1   AACATGCT...
+        #   zotu2   AAGATCCT...
+        seq_df = self._fasta_to_df()
+
+        for otu in otus:
+            zids = np.append(clusts.loc[otu].values, otu)
+            seqs = seq_df.loc[zids].values
+            with open(f"{tmp_fastadir}/{otu}.fasta", 'w') as outfile:
+                for idx, seq in enumerate(seqs):
+                    outfile.write(f">{otu}_{idx}\n{seq}\n")
+
+        if verbose: print("Aligning..")
+        self._muscle_cmds = []
+        for fa in glob.glob(f"{tmp_fastadir}/*.fasta"):
+            outfile = f"{fa}.aln"
+            cmd = ["muscle",
+                    "-align", f"{fa}",
+                    "-output", f"{outfile}",
+                    "-quiet",
+                    "-threads", "2"]
+            self._muscle_cmds.append(' '.join(cmd))
+
+        # Run all muscle commands in parallel
+        results = joblib.Parallel(n_jobs=20)(joblib.delayed(\
+                                    self._systemcall)(f) for f in self._muscle_cmds)
+
+        # Gather all aligned fastas into one file
+        # The muscle output breaks sequence data at 80 characters, so we have
+        # to recompose it into one long line
+        aligned = f"{tmp_fastadir}/combined-aligned-{OTU_threshold}.fasta"
+        with open(aligned, 'w') as outfile:
+            for fa in glob.glob(f"{tmp_fastadir}/*.aln"):
+                dat = open(fa).read().split()
+                for line in dat:
+                    if ">" in line:
+                        outfile.write(f"\n{line}\n")
+                    else:
+                        outfile.write(line)
+
+        # Remove the leading newline. This is dumb, but it works
+        dat = open(aligned).readlines()
+        with open(aligned, 'w') as outfile:
+            outfile.write("".join(dat[1:]).strip())
+
+        return aligned
+
+
+    def _fasta_to_df(self):
+        seq_data = open(self.data).read().split()
+        ## Doing some formatting to make metadata and fasta zotu names agree:
+        # Drop the leading >
+        # Force all lowercase
+        # Remove trailing underscore
+        zotus = [x[1:] for x in seq_data[::2]]
+        # Strip any trailing gaps (not sure why they are there)
+        fastas = [x.rstrip("-") for x in seq_data[1::2]]
+
+        return pd.Series(fastas, index=zotus)
+
+
 ###################
 # Utility functions
 ###################
 
-def plot_multi(bci_list, ax=None, log=True, normalize=False, cmap="Spectral", **kwargs):
+def plot_multi(bci_list, ax=None, log=True, normalize=False, plot_pis=False, cmap="Spectral", keyed_cmaps=None, **kwargs):
     """
     Plot multiple BCIs from different datasets
+
+    keyed_cmaps (dict) - Keys are substrings common to groups of samples
+                        (site name, habititat, etc), values are cmaps to use
     """
     if not ax:
         fig, ax = plt.subplots(figsize=(8, 8))
+    else:
         fig = ax.get_figure()
 
+    if keyed_cmaps:
+        # Use one coloramp for each group of samples keyed by identifiers in the sample names
+        cdict = {}
+        for k, v in keyed_cmaps.items():
+            bci_labels = [x.samp for x in bci_list if k in x.samp]
+            cmap = cm.get_cmap(v)(np.linspace(0.3, 0.9, len(bci_labels)))
+            cdict.update({x:cmap[i] for i, x in enumerate(bci_labels)})
+    else:
+        # use one colormap for all samples
         cmap = cm.get_cmap(cmap)
         # Get a dictionary mapping bcis to evenly spaced color values in the cmap
-        bci_labels = sorted(set([x.samp for x in bci_list]))
+        bci_labels = [x.samp for x in bci_list]
         cdict = {x:cmap(i/len(bci_labels)) for i, x in enumerate(bci_labels)}
 
-        for bci in bci_list:
+    for bci in bci_list:
+        if plot_pis:
+            try:
+                data = np.array(sorted(bci.pis.values(), reverse=True))
+            except:
+                print("  BCI does not have pi data, giving up the plot.")
+                return None, None
+        else:
             # Raw results for untransformed data will be saved in the results
             # dict keyed by the sample name
             data = bci._results[bci.samp][0]
             # log transform
-            data = np.log(data) if log else data
-            # normalize
-            norm = data[0] if normalize else 1
-            ax.plot(np.array(data)/norm, label=bci.samp, color=cdict[bci.samp], **kwargs)
-        ax.legend(loc='upper right', bbox_to_anchor=(0.97, 0.97), labels=bci_labels)
-        return fig, ax
+            data = np.log(data) if log else np.array(data)
+        # normalize
+        norm = data.sum() if normalize else 1
+        ax.plot(data/norm, label=bci.samp, color=cdict[bci.samp], **kwargs)
+    #ax.legend(loc='upper right', bbox_to_anchor=(0.97, 0.97), labels=bci_labels)
+    plt.legend()
+    return fig, ax
 
 
 def phylip_to_fasta(phylip, verbose=False):
